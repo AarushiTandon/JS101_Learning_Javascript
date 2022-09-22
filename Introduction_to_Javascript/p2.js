ed17f85a5d8a43c8f0573c3c0007ce38a094442d
unknown <aarushitandon000@gmail.com>
2022-09-22 18:56:32 +0530

    git-I project

---
+++ Introduction_to_Javascript/p2.js
@@ -0,0 +1,18 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name="Aarushi";
+console.log(name);
+name ="Alok";
+console.log(name);
+name ="Asha";
+console.log(name);
+
+
+
+
---
+++ Introduction_to_Javascript/p4.js
@@ -0,0 +1,25 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let name ="Aarushi";
+let school_name ="Masai School";
+let grade ="A";
+let sec ="c";
+let roll_num = 14;
+let Maths_marks = 72;
+let English_marks = 74;
+let Comp_marks = 78;
+console.log("____𝑹𝒆𝒑𝒐𝒓𝒕 𝑪𝒂𝒓𝒅____");
+console.log(
+  "Name:-"+" "+name+"\n"+
+ "School_name:-"+" "+school_name+"\n"+
+ "Grade:-"+" "+grade+"\n"+
+  "Section:-"+" "+sec+"\n"+
+ "Roll_num:-"+" "+roll_num+"\n"+
+ "Maths_marks:-"+" "+Maths_marks+"\n"+
+ "English_marks:-"+" "+English_marks+"\n"+
+ "Computer_marks:-"+" "+Comp_marks
+)
+
+
